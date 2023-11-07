fe9cd407542062ac57a1bb53d29bd1b531c11059
Raj <SoftDevRaj77@gmail.com>
2023-11-06 21:08:10 -0500

    updated post.js file

--- models/Post.js
+++ models/Post.js
@@ -1,32 +1,26 @@
-// Post.js in the models folder
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// Create the Post model
+// create our Post model
 class Post extends Model {}
 
-// Define table columns and configuration for Post model
+// create fields/columns for Post model
 Post.init(
   {
-    // Define an id column
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    // Define a title column
     title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // Define a content column for the post body
     content: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    // The user_id column references the id from the User model, creating a foreign key
     user_id: {
       type: DataTypes.INTEGER,
       references: {
