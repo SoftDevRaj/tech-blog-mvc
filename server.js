d0a5e897c5b9932fb83cd66c16e0c89dccc8febe
Raj <SoftDevRaj77@gmail.com>
2023-11-06 20:22:08 -0500

    created auth.js and updated server.js

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
+// server.js
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
-// Import sequelize connection
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Import database connection from config folder
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Import custom middleware
+const withAuth = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ /* Your helpers here */ });
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Configure and connect to the session store
+// Set up sessions with cookies
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Your Secret', // replace with your own secret
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -26,23 +32,16 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+// Set up Handlebars.js as the default template engine
+const hbs = exphbs.create({}); // You can add custom handlebar helpers or configurations if needed
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', './views');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// Turn on routes
+const routes = require('./controllers'); // this will require an index.js file in your controllers directory that handles your route definitions
+app.use(routes);
 
-// Require routes
-// Here you will require your routes files when you have created them in next steps
-// const routes = require('./controllers/...');
-// app.use(routes);
-
-// Add a simple route for testing your application
-app.get('/', (req, res) => res.send('Hello World!'));
-
+// Sync sequelize models to the database and then start Express app
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 });
